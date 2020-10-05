abe2331b2ec124e0fd9ea598efb3e1be392f8a57
Alumno <cesarrgomez@hotmail.com>
2020-10-05 15:34:47 -0700

    Primer Commit

---
+++ Propinas/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Propinas
+//
+//  Created by Alumno on 10/5/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lblporcentaje: UILabel!
+    @IBAction func doChangeporcentaje(_ sender: Any) {
+        lblporcentaje.text = 
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
